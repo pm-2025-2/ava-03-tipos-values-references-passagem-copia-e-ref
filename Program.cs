603a804d245672d160f983bc6d43a1c77d3ff4de
Marcio <marcio.torres@riogrande.ifrs.edu.br>
2025-11-10 10:22:33 -0300

    ava

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Uma Localização tem Latitude e Longitude armazenadas em decimal
+// A Latitude deve estar entre -90 e 90
+// A Longitude deve estar entre -180 e 180
+
+// Criar métodos/funções que movem uma Localização para Norte, Sul, Leste e Oeste
+// Cada movimento deve receber um parâmetro decimal que indica a distância a ser movida
+// Ex: Mover Norte 10.5 aumenta a Latitude em 10.5
+// Se a Latitude ou Longitude ultrapassar os limites, deve girar para o lado oposto
+// Ex: Se a Latitude for 95, deve ser ajustada para -85
+
+// Implementar várias versões dessas funções, considerando o uso de classe (ou record class), uso de struct (ou record struct), com e sem return, passando por cópia e por referência.
